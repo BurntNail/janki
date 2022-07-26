b85e8da16a5c846a6ccfd1a8252c6f42023232ea
Jack Maguire <jackmaguire1234@gmail.com>
2022-07-26 09:21:39 +0100

    remove jaeger

--- src/egui/main.rs
+++ src/egui/main.rs
@@ -3,22 +3,14 @@ extern crate tracing;
 
 use crate::janki_app::JankiApp;
 use eframe::NativeOptions;
-use tracing_subscriber::layer::SubscriberExt;
 
 mod janki_app;
 
 fn main() {
-    //copied from https://fasterthanli.me/articles/when-rustc-explodes#tracing-deeper-with-debug-spans
-    let tracer = opentelemetry_jaeger::new_pipeline()
-        .with_service_name("janki_egui")
-        .with_collector_endpoint("http://localhost:14268/api/traces")
-        .install_simple()
-        .unwrap();
-
-    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-
-    let subscriber = tracing_subscriber::Registry::default().with(telemetry);
-    tracing::subscriber::set_global_default(subscriber).unwrap();
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     eframe::run_native(
         "Janki",
