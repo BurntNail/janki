375903d3226bf50fc8c5b19d33e0306b94103c4e
Jack Maguire <jackmaguire1234@gmail.com>
2022-07-29 22:41:24 +0100

    csv docs

--- src/csv.rs
+++ src/csv.rs
@@ -1,6 +1,9 @@
 use crate::item::Fact;
 use std::{error::Error, fs::File, io::Write};
 
+///Function to read in a `Vec<Fact>` from a set path.
+///
+///Can return an [`std::error::Error`] if either the file cannot be read or parsed
 #[instrument]
 pub fn read_in(path: &str) -> Result<Vec<Fact>, Box<dyn Error>> {
     info!("Reading in");
@@ -22,8 +25,11 @@ pub fn read_in(path: &str) -> Result<Vec<Fact>, Box<dyn Error>> {
         .collect())
 }
 
+///Writes out a `Vec<Fact>` to a set path, overwriting any current contents.
+/// 
+///Can return an [`std::io::Error`] if the file cannot be written
 #[instrument(skip(db), fields(db_len = ?db.len()))]
-pub fn write_out(path: &str, mut db: Vec<Fact>) -> Result<(), Box<dyn Error>> {
+pub fn write_out(path: &str, mut db: Vec<Fact>) -> Result<(), std::io::Error> {
     db.sort();
     db.dedup();
     info!("Writing list");
