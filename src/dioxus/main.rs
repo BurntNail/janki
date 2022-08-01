463362e20f4b8317e9074699377141bb304938c4
Jack Maguire <jackmaguire1234@gmail.com>
2022-08-01 22:33:45 +0100

    hello world for dioxus

---
+++ src/dioxus/main.rs
@@ -0,0 +1,13 @@
+﻿use dioxus::prelude::*;
+
+fn main() {
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "hello world!"
+        }
+    })
+}
